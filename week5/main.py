e2ff58f575331a4ccf32ce1bd62aca006889731c
perthoyra <per.thoyra@outlook.com>
2026-05-18 09:54:17 +0200

    matplotlib

---
+++ week5/main.py
@@ -0,0 +1,13 @@
+import matplotlib.pylot as plt
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
+
+def main():
+    print("Hello World!")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()

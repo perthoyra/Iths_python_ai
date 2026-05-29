6eb379b3be417f5ac35db31e5d4f7f3c3cae15d6
perthoyra <per.thoyra@outlook.com>
2026-05-29 12:35:06 +0200

    pandas simple example

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Iths_python_ai
+# Various tests and examples of using pythong for AI
--- pandas/main.py
+++ pandas/main.py
@@ -10,8 +10,27 @@ def run():
 
     name_series = df["Name"]
     print(name_series)
+    
+    # use df.notna() to get the opposite result, shows where a value is missing but harder to read
+    missing_values = df.isna().sum()
 
-    print(df["Score"].mean())
+    print(missing_values)
+
+    # test to see if we can get the invalid/missing value
+    for col_value in missing_values:
+        if col_value != 0:
+                print(col_value)
+
+    # replace value in the in menory Copy of the data
+    # NOTE: fillna() casts int as floats
+    df["Score"] = df["Score"].fillna(0)
+    df["Score"] = df["Score"].astype(int)
+
+    print(df["Score"])
+    print(df.isna().sum())
+
+    df.to_csv(f"{DATA_DIR}/clean_data.csv", index=False)
+
 
 if __name__ == "__main__":
     run()

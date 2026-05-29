9279df3861b90074acc31e28ebe69f5dce0f5921
perthoyra <per.thoyra@outlook.com>
2026-05-29 11:49:39 +0200

    read csv,all correct

--- pandas/main.py
+++ pandas/main.py
@@ -1,8 +1,17 @@
+from pathlib import Path
 import pandas as pd
 
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
+
 def run():
-    df = pd.read_csv("data/students.csv")
+    df = pd.read_csv(f"{DATA_DIR}/students.csv")
     print(df)
+
+    name_series = df["Name"]
+    print(name_series)
+
+    print(df["Score"].mean())
 
 if __name__ == "__main__":
     run()

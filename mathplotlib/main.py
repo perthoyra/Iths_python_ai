5ca0cdab1afcaa97ea0152607736013682b3f894
perthoyra <per.thoyra@outlook.com>
2026-05-29 14:53:03 +0200

    interactive plot

--- mathplotlib/main.py
+++ mathplotlib/main.py
@@ -1,11 +1,10 @@
 # %%
-
 import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
 
-BASE_DIR = Path(__file__).cwd()
-## BASE_DIR = Path(__file__).resolve().parent <- Does not work with jupytr notebooks
+# BASE_DIR = Path(__file__).cwd()
+BASE_DIR = Path(__file__).resolve().parent # <- Does not work with jupytr notebooks
 DATA_DIR = BASE_DIR / "data"
 OUTPUTS_DIR = BASE_DIR / "outputs"
 MODELS_DIR = BASE_DIR / "models"
@@ -17,10 +16,29 @@ OUTPUTS_DIR.mkdir(exist_ok=True)
 
 # %%
 
+# Here we are doing a lot of validation and verifying our data
+# ......
+# we do not do this since we have perfect data in this case :)
+
+#set up the graf plot
 df = pd.read_csv(DATA_DIR / "coffee_sales.csv")
 
-plt.plot(df["day"], df["cups_sold"] )
-plt.tile("Cups sold per day")
+x_values = df["day"]
+y_values = df["cups_sold"]
+
+fig, ax = plt.subplots(figsize=(7,4))
+
+#setup the plot, values and labels
+ax.plot(x_values, y_values, marker="o")
+ax.set_title("Cups sold per day")
+ax.set_xlabel("Day")
+ax.set_xlabel("Number of cups")
+
+# create the fig ( picture )
+fig.tight_layout()
+
+# remember fig and ax are references to objects in plt, created on row 30
 plt.show()
+plt.close(fig)
 
 # %%
